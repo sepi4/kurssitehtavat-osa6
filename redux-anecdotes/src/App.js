cc8091b7d3933fd08e0e7aa77f109bf47193b178
Sergei <sepolish@student.jyu.fi>
2019-09-26 05:03:09 +0300

    6.8: anekdootit, step6

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,33 +1,15 @@
 import React from 'react';
 
 import AnecdoteForm from './components/AnecdoteForm'
-
-import { voteAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteList from './components/AnecdoteList'
 
 
 
 const App = (props) => {
-  const anecdotes = props.store.getState()
-
-  const vote = (id) => {
-    props.store.dispatch(voteAnecdote(id))
-  }
-
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList store={props.store} />
       <AnecdoteForm store={props.store} />
     </div>
   )
