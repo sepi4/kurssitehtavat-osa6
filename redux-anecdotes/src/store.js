a224c6cd951136ab5fc0e1abb3c50103d61a7cf1
Sergei <sepolish@student.jyu.fi>
2019-09-27 04:20:52 +0300

    6.18 anekdootit ja backend, step4

---
+++ redux-anecdotes/src/store.js
@@ -0,0 +1,19 @@
+import anecdoteReducer from './reducers/anecdoteReducer'
+import notificationReducer from './reducers/notificationReducer'
+import filterReducer from './reducers/filterReducer'
+
+import thunk from 'redux-thunk'
+
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+
+
+
+const reducer = combineReducers({
+  anecdotes: anecdoteReducer,
+  notification: notificationReducer,
+  filter: filterReducer,
+})
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+export default store
